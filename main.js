6069f4f74a3d7eb942b67e42739f9ccb42626d86
wizardozi <alexdenommee@gmail.com>
2025-06-29 17:14:40 -0700

    Add electron shell

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
